deb2a06d56d1780b420d12f65f975d5ddaa38a9a
Jacob Bruce <jacobwbruce@hotmail.com>
2024-01-23 14:36:17 -0500

    error plugins for nvim

---
+++ nvim/lua/plugins/errors.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "chikko80/error-lens.nvim",
+    event = "BufRead",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+      -- your options go here
+    },
+  },
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+      require("lsp_lines").setup()
+      -- vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
+      -- set updatetime=1000
+      vim.cmd([[autocmd! CursorHold * lua vim.diagnostic.show_line_diagnostics({ focusable = false })]])
+      vim.cmd(
+        [[autocmd! CursorHold * lua vim.diagnostic.config({ virtual_lines = { only_current_line = true } })]]
+      )
+    end,
+  },
+}
