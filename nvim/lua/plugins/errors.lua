4334d44405fbefd79bc70cbd34d5dd6ab00b6f83
Jacob Bruce <jacobwbruce@hotmail.com>
2024-01-31 14:01:16 -0500

    some git & error changes

--- nvim/lua/plugins/errors.lua
+++ nvim/lua/plugins/errors.lua
@@ -9,16 +9,16 @@ return {
       -- your options go here
     },
   },
-  {
-    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-    config = function()
-      require("lsp_lines").setup()
-      -- vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
-      -- set updatetime=1000
-      vim.cmd([[autocmd! CursorHold * lua vim.diagnostic.show_line_diagnostics({ focusable = false })]])
-      vim.cmd(
-        [[autocmd! CursorHold * lua vim.diagnostic.config({ virtual_lines = { only_current_line = true } })]]
-      )
-    end,
-  },
+  -- {
+  --   "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  --   config = function()
+  --     require("lsp_lines").setup()
+  --     -- vim.diagnostic.config({ virtual_lines = { only_current_line = true } })
+  --     -- set updatetime=1000
+  --     vim.cmd([[autocmd! CursorHold * lua vim.diagnostic.show_line_diagnostics({ focusable = false })]])
+  --     vim.cmd(
+  --       [[autocmd! CursorHold * lua vim.diagnostic.config({ virtual_lines = { only_current_line = true } })]]
+  --     )
+  --   end,
+  -- },
 }
--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,17 +1,25 @@
 return {
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup({
-				signs = {
-					add = { text = "│" },
-					change = { text = "│" },
-					delete = { text = "󰍵" },
-					topdelete = { text = "‾" },
-					changedelete = { text = "~" },
-					untracked = { text = "│" },
-				},
-			})
-		end,
-	},
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add = { text = "│" },
+          change = { text = "│" },
+          delete = { text = "󰍵" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+          untracked = { text = "│" },
+        },
+      })
+
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {
+        desc = "Preview hunk",
+      })
+
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {
+        desc = "Toggle current line blame",
+      })
+    end,
+  },
 }
