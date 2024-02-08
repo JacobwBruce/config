cc5f7f91f47e235f6751bf420ebb757387d76050
Jacob Bruce <jacobwbruce@hotmail.com>
2024-02-08 12:43:46 -0500

    python setup

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,117 +1,122 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local util = require("lspconfig/util")
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local util = require("lspconfig/util")
 
-			local lspconfig = require("lspconfig")
+      local lspconfig = require("lspconfig")
 
-			local function organize_imports()
-				local params = {
-					command = "_typescript.organizeImports",
-					arguments = { vim.api.nvim_buf_get_name(0) },
-				}
-				vim.lsp.buf.execute_command(params)
-			end
+      local function organize_imports()
+        local params = {
+          command = "_typescript.organizeImports",
+          arguments = { vim.api.nvim_buf_get_name(0) },
+        }
+        vim.lsp.buf.execute_command(params)
+      end
 
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-				init_options = {
-					preferences = {
-						disableSuggestions = true,
-					},
-				},
-				commands = {
-					OrganizeImports = {
-						organize_imports,
-						desc = "Organize Imports",
-					},
-				},
-			})
-			lspconfig.tailwindcss.setup({
-				capabilities = capabilities,
-				cmd = { "tailwindcss-language-server", "--stdio" },
-				filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" },
-				root_dir = util.root_pattern("tailwind.config.js", "package.json", "postcss.config.js", ".git"),
-			})
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+        init_options = {
+          preferences = {
+            disableSuggestions = true,
+          },
+        },
+        commands = {
+          OrganizeImports = {
+            organize_imports,
+            desc = "Organize Imports",
+          },
+        },
+      })
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+        cmd = { "tailwindcss-language-server", "--stdio" },
+        filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+        root_dir = util.root_pattern("tailwind.config.js", "package.json", "postcss.config.js", ".git"),
+      })
 
-			lspconfig["emmet_ls"].setup({
-				capabilities = capabilities,
-				filetypes = { "html", "css", "scss", "sass", "javascriptreact", "less", "typescriptreact", "svelte" },
-			})
+      lspconfig["emmet_ls"].setup({
+        capabilities = capabilities,
+        filetypes = { "html", "css", "scss", "sass", "javascriptreact", "less", "typescriptreact", "svelte" },
+      })
 
-			lspconfig.prismals.setup({
-				capabilities = capabilities,
-				filetypes = { "prisma" },
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.cssls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.gopls.setup({
-				capabilities = capabilities,
-				settings = {
-					gopls = {
-						completeUnimported = true,
-						usePlaceholders = true,
-						analyses = {
-							unusedparams = true,
-						},
-					},
-				},
-			})
+      lspconfig.prismals.setup({
+        capabilities = capabilities,
+        filetypes = { "prisma" },
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+        settings = {
+          gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+              unusedparams = true,
+            },
+          },
+        },
+      })
 
-			lspconfig.solargraph.setup({
-				capabilities = capabilities,
-				cmd = { "solargraph", "stdio" },
-				filetypes = { "ruby" },
-				root_dir = function(fname)
-					return util.root_pattern("Gemfile", ".git")(fname) or util.path.dirname(fname)
-				end,
-				settings = {
-					solargraph = {
-						diagnostics = true,
-					},
-				},
-			})
+      lspconfig.solargraph.setup({
+        capabilities = capabilities,
+        cmd = { "solargraph", "stdio" },
+        filetypes = { "ruby" },
+        root_dir = function(fname)
+          return util.root_pattern("Gemfile", ".git")(fname) or util.path.dirname(fname)
+        end,
+        settings = {
+          solargraph = {
+            diagnostics = true,
+          },
+        },
+      })
 
-			lspconfig.jdtls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.jdtls.setup({
+        capabilities = capabilities,
+      })
 
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {
-				desc = "Show hover",
-			})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {
-				desc = "Go to definition",
-			})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {
-				desc = "Go to references",
-			})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {
-				desc = "Code action",
-			})
-		end,
-	},
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        filetypes = { "python" },
+      })
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {
+        desc = "Show hover",
+      })
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {
+        desc = "Go to definition",
+      })
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {
+        desc = "Go to references",
+      })
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {
+        desc = "Code action",
+      })
+    end,
+  },
 }
