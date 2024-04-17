4e0fba1ed0780057b19deb8b58f90dec9a003555
Jacob Bruce <jacobwbruce@hotmail.com>
2024-04-17 13:27:00 -0400

    neo tree UI changes

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,29 +1,30 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	opts = {
-		filesystem = {
-			filtered_items = {
-				hide_dotfiles = false,
-				hide_gitignore = false,
-			},
-		},
-		window = {
-			auto_expand_width = true,
-		},
-	},
-	config = function(_, opts)
-		require("neo-tree").setup(opts)
-		vim.keymap.set(
-			"n",
-			"<C-n>",
-			":Neotree toggle filesystem reveal left<CR>",
-			{ silent = true, desc = "Toggle NeoTree" }
-		)
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignore = false,
+      },
+    },
+    window = {
+      auto_expand_width = true,
+    },
+    hide_root_node = true,
+  },
+  config = function(_, opts)
+    require("neo-tree").setup(opts)
+    vim.keymap.set(
+      "n",
+      "<C-n>",
+      ":Neotree toggle filesystem reveal left<CR>",
+      { silent = true, desc = "Toggle NeoTree" }
+    )
+  end,
 }
