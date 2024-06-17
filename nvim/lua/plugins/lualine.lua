6c3dac8a04e5f8438ac6d43e1b6aec23d5dffc72
Jacob Bruce <jacobwbruce@hotmail.com>
2024-06-17 15:36:50 -0400

    tokyo night theming

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,10 +1,11 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'ayu_mirage'
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				-- theme = 'ayu_mirage'
+				theme = "tokyonight",
+			},
+		})
+	end,
 }
--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,37 +1,52 @@
 return {
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catpuccin",
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			require("rose-pine").setup({
-				variant = "moon",
-			})
-			-- vim.cmd.colorscheme("rose-pine")
-		end,
-	},
-	{
-		"bluz71/vim-nightfly-colors",
-		name = "nightfly",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			-- vim.cmd.colorscheme("nightfly")
-		end,
-	},
-	{
-		"embark-theme/vim",
-		name = "embark",
-		config = function()
-			-- vim.cmd.colorscheme("embark")
-		end,
-	},
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catpuccin",
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        variant = "moon",
+      })
+      -- vim.cmd.colorscheme("rose-pine")
+    end,
+  },
+  {
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- vim.cmd.colorscheme("nightfly")
+    end,
+  },
+  {
+    "embark-theme/vim",
+    name = "embark",
+    config = function()
+      -- vim.cmd.colorscheme("embark")
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+        on_colors = function(colors)
+          -- colors.green = "#7AE582"
+          colors.green = "#85e6a9"
+        end,
+      })
+      vim.cmd([[colorscheme tokyonight-night]])
+    end,
+  },
 }
