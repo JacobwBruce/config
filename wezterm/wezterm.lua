ecabd0f74acf2725575748b90a4add8244382d47
Jacob Bruce <jacobwbruce@hotmail.com>
2024-08-20 10:41:41 -0400

    initial zephyr prompt

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.font = wezterm.font("JetBrainsMonoNL Nerd Font Propo")
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font Propo", { weight = "DemiBold" })
 config.font_size = 14
 
 config.enable_tab_bar = false
